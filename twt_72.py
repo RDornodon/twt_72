4416fbdfa9c0ad01a9f0352f395fd3476e5c335a
RDornodon <archelf.hun@gmail.com>
2021-09-21 21:38:30 +0200

    flex, tester

--- twt_72.py
+++ twt_72.py
@@ -1,4 +1,3 @@
-for I in[I:=input]*int(I()):
- D=I();r,l=0>1,len(D)
- for v in range(1,l):r|=D==''.join(map(str,range(i:=int(D[:v]),i+l//v)))
+for r in[0>1]*int(input()):
+ for v in range(1,l:=len(D:=input())):r|=D==''.join(map(str,range(i:=int(D[:v]),i+l//v)))
  print(r)
